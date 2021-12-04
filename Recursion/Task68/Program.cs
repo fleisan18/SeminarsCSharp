01a1803facf7ff85f375bc0ad8c39315e9087539
Leisan <fleisan18@mail.ru>
2021-12-04 19:50:49 +0500

    add task 68

---
+++ Recursion/Task68/Program.cs
@@ -0,0 +1,19 @@
+﻿// 68. Показать натуральные числа от M до N, N и M заданы
+string ShowNumbers(int m, int n)
+{
+    if (m == n) return $"{n}";
+    else
+    {
+        if (m > n) return $"{m} {ShowNumbers(m - 1, n)}";
+        if (m < n) return $"{m} {ShowNumbers(m + 1, n)}";
+    }
+    return $"{ShowNumbers(m,n)}";
+}
+System.Console.WriteLine("Введите число M");
+int M = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(ShowNumbers(M, N));
+
