bfa1023b5dec1e661d15feb10c554cf7449a9ce7
Leisan <fleisan18@mail.ru>
2021-12-04 19:29:36 +0500

    add task 67

---
+++ Recursion/Task67/Program.cs
@@ -0,0 +1,8 @@
+﻿// 67. Показать натуральные числа от N до 1, N задано
+string ShowNumbers(int n)
+{
+    if (n == 1) return "1";
+    else return $" {n} {ShowNumbers(n - 1)}";
+}
+System.Console.WriteLine(ShowNumbers(15));
+
