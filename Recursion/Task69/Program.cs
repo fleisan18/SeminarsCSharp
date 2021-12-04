d3e00cb13ef713538dc8435af3fcd0804e1ad1a3
Leisan <fleisan18@mail.ru>
2021-12-04 20:02:26 +0500

    add task 69

---
+++ Recursion/Task69/Program.cs
@@ -0,0 +1,22 @@
+﻿// 69. Найти сумму элементов от M до N, N и M заданы
+
+int Sum (int m, int n)
+{
+    if(m==n) return n;
+    else
+    {
+        if(m<n) return m+Sum(m+1,n);
+        else return m+Sum(m-1, n);
+    }
+}
+
+
+System.Console.WriteLine("Введите число M");
+int M = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+int result = (Sum(M,N));
+System.Console.WriteLine(result);
