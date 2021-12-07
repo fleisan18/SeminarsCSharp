5f58a9189993056ffa34dc7af75475757fb68f51
Leisan <fleisan18@mail.ru>
2021-12-07 11:17:34 +0500

    task 49

---
+++ Two-dimensional arrays/Task49/Program.cs
@@ -0,0 +1,34 @@
+﻿// 49.Показать двумерный массив размером m×n заполненный вещественными числами
+int M = 10;
+int N = 3;
+double[,] CreateArray (int m, int n)
+{
+    double[,] array = new double [m,n];
+    return array;
+}
+
+double[,] FillArray (double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = Math.Round((new Random().NextDouble()*100),3);
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+PrintArray(FillArray(CreateArray(M, N)));
